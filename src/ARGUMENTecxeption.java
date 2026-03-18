d1bc0dcf6e3be8a6285762792ebf24104d2f2abf
malayvatsa <24051181@kiit.ac.in>
2026-03-19 00:44:46 +0530

    init commit

---
+++ src/ARGUMENTecxeption.java
@@ -0,0 +1,29 @@
+class CheckArgument extends Exception {
+    CheckArgument(String msg) {
+        super(msg);
+    }
+}
+
+class ArgDemo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            if (args.length < 4)
+                throw new CheckArgument("CheckArgument");
+
+            int sum = 0;
+
+            for (int i = 0; i < 4; i++) {
+                int n = Integer.parseInt(args[i]);
+                sum = sum + (n * n);
+            }
+
+            System.out.println(sum);
+
+        } catch (CheckArgument e) {
+            System.out.println("Exception occurred - " + e.getMessage());
+        }
+    }
+}
